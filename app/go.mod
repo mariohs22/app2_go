148a7199894deb25f7cf104fbcb2b9311f5b849f
Mariohs22 <serge.burakovsky@gmail.com>
2021-07-28 14:19:12 +0300

    app: add test script

--- app/go.mod
+++ app/go.mod
@@ -2,4 +2,8 @@ module helloworld
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/go-playground/assert/v2 v2.0.1
+	github.com/stretchr/testify v1.4.0 // indirect
+)
--- app/main.go
+++ app/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func HelloWorld(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title": "Hello world 2",
+	})
+}
+
 func main() {
 	r := gin.Default()
 	r.LoadHTMLFiles("index.html")
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Hello world 2",
-		})
-	})
+	r.GET("/", HelloWorld)
+
 	err := r.Run()
 	if err != nil {
-		fmt.Print("handle your error here")
+		panic(err)
 	}
 }
